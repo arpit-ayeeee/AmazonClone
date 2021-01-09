f1e287ea3868fc32df50eba18c92771ee07b7afb
Arpit Jaiswal <arpitjaiswal282000@gmail.com>
2021-01-09 19:54:11 +0530

    Home page half done

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,28 @@
-import React from 'react'
+import React from 'react';
 import './Home.css';
+import Product from './Product';
 
 function Home() {
     return (
         <div className="home">
-            I am the home component
+            <div className="home_container">
+                <img className="home_image" src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg" alt="banner"/>
+                <div className="home_row">
+                    <Product id="123456" title="The lean startup" price={29.99} image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg" rating={3}/>
+                    <Product id="49538094" title="Mixer & Grinder" price={299.99} image="https://images-na.ssl-images-amazon.com/images/I/810%2BGNdkzKL._AC_SX450_.jpg" rating={3}/>
+                </div>
+                <div className="home_row">
+                    <Product id="" title="The lean startup" price={29.99} image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg" rating={3}/>
+                    <Product id="" title="The lean startup" price={29.99} image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg" rating={3}/>
+                    <Product id="" title="The lean startup" price={29.99} image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg" rating={3}/>
+                </div>
+                <div className="home_row">
+                    <Product id="" id="" title="The lean startup" price={29.99} image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg" rating={3}/>
+                </div>
+            </div>
         </div>
     )
 }
 
 export default Home
+
---
+++ src/Product.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Product.css';
+
+function Product({title,image,price,rating}) {
+    return (
+        <div className="product">
+            <div className="product_info">
+                <p>{title}</p>
+                <p className="product_price">
+                    <small>$</small>
+                    <strong>{price}</strong>
+                </p>
+                <div className="product_rating">
+                    {Array(rating)
+                        .fill()
+                        .map((_, i) => (
+                        <p>🌟</p>
+                    ))}
+                </div>
+            </div>
+            <img className="product_image"src={image} alt="" />
+            <button>Add to basket</button>
+        </div>
+    )
+}
+
+export default Product;
